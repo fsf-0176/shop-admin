1d503642b2419120eb250c49b81a788872d0a7d0
天青色等烟雨 <5250322+fangsifa99@user.noreply.gitee.com>
2021-03-19 23:54:16 +0800

    新增

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -48,22 +48,72 @@
         <p>姓名：大侠</p>
         <p>手机：13888888888</p>
       </div>
-      <div class="address"></div>
-      <div class="detail"></div>
+      <div class="address">
+        <p>名称 13888888888</p>
+        <small>天津市天津市和平区测试地址</small>
+        <el-input
+          type="textarea"
+          :rows="2"
+          placeholder="请输入内容"
+          v-model="textarea"
+        >
+        </el-input>
+      </div>
+      <div class="detail">
+        <a href="#">查看详情</a>
+      </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      textarea: ''
+    }
+  }
+}
+</script>
 <style lang="less" scoped>
 .orderList {
   width: 100%;
   border: 1px solid #e7e7e7;
+  .detail{
+    width: 15%;
+    a{
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+      justify-content: center;
+      color: #409eff;
+      font-size: 12px;
+    }
+  }
+  .address {
+    padding: 15px;
+    border-right: 1px solid #e7e7e7;
+    width: 30%;
+    p{
+      margin: 15px 0;
+    }
+    small{
+      display: block;
+      margin-bottom: 15px;
+    }
+  }
   .content {
+    font-size: 14px;
     display: flex;
-    .user{
-      .pic{
+    .user {
+      padding: 15px;
+      border-right: 1px solid #e7e7e7;
+      width: 25%;
+      .pic {
         display: flex;
         align-items: center;
-        .name{
+        margin: 10px 0;
+        .name {
           display: inline-block;
           margin-left: 10px;
         }
@@ -77,7 +127,7 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        font-size: 14px;
+
         border-bottom: 1px solid #e7e7e7;
         .picture {
           display: flex;
