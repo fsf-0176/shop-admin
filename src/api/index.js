92604c74c0043a8da546eb91e6ecc7d48ae48912
天青色等烟雨 <405719102@qq.com>
2021-03-30 23:52:01 +0800

    封装axios

---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+
+axios.default.baseUrl = process.env.VUE_APP_SERVER_API_PATH
+// 请求发送前
+axios.interceptors.request.use(configs => {
+  try {
+    const data = JSON.parse(localStorage.getItem('user'))
+    if (data.authorization) {
+      configs.headers.common.authorization = data.authorization
+    }
+  } catch (error) {
+
+  }
+  return configs
+})
+axios.interceptors.response.use(response => {
+  return response
+},
+  error => {
+    alert('出错了')
+  }
+)
